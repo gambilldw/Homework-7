bd9ba86d0b1f198e66f5fd6249b890f68c9d5227
gambilldw <gambilldw@gmail.com>
2018-03-05 23:45:55 -0500

    Add files via upload

---
+++ Homework 7/logic.js
@@ -0,0 +1,61 @@
+var config = {
+    apiKey: "AIzaSyDr59EP-eiDJTAKONwHG-0-AtWYzurd7hk",
+    authDomain: "fir-train-homework.firebaseapp.com",
+    databaseURL: "https://fir-train-homework.firebaseio.com",
+    projectId: "fir-train-homework",
+    storageBucket: "fir-train-homework.appspot.com",
+    messagingSenderId: "42340729487"
+};
+
+firebase.initializeApp(config);
+
+var trainData = firebase.database();
+
+$("addTrainBtn").on("click", function() {
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+    var frequency = $("#frequencyInput").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    }
+
+    trainData.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+
+
+    alert("Train Added!");
+
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainInput").val("");
+    $("#frequencyInput").val("");
+
+    return false;
+})
+
+
+trainData.ref().on("child_added", function (snapshot) {
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
+
+    var remainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes, "m").format("hh:mm A");
+
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival);
+
+    $("#trainTable > tBody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
+});
